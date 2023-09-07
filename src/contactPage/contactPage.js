c5094bac87d6ec2e8533678f85ca958d274702fe
Tu Vu <vuanhtu52@gmail.com>
2023-09-07 23:18:18 +0700

    Complete contact page

--- src/contactPage/contactPage.js
+++ src/contactPage/contactPage.js
@@ -2,7 +2,16 @@ import "./contactPage.css";
 
 const createContactPage = () => {
     const contactPage = document.createElement("div");
-    contactPage.textContent = "contact page";
+    contactPage.className = "contact-page";
+    
+
+    const phone = document.createElement("div");
+    phone.textContent = "Phone number: 0123-456-789.";
+    contactPage.appendChild(phone);
+    
+    const address = document.createElement("div");
+    address.textContent = "Address: 1234 Street, NY, USA.";
+    contactPage.appendChild(address); 
 
     return contactPage;
 }
